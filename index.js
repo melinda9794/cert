26f5d42e3e5b3b8e731d9f4d745348ddbfe8aa4e
Dale Yarborough <daleyarborough@gmail.com>
2017-06-22 14:25:03 -0400

    Create index.js

---
+++ index.js
@@ -0,0 +1,9 @@
+(function(angular) {  
+  'use strict';\
+  
+  angular.module('heroApp', []).controller('MainCtrl', function MainCtrl() {  
+    this.hero = {    
+      name: 'Spawn'  
+      };
+   });
+})(window.angular);
